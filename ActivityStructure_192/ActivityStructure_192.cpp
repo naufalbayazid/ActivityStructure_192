3a361cb7365b95f43967c2dfbd0f5ddd6b4fcb37
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-04-03 14:46:27 +0700

    masukkan data mahasiswa 1

--- ActivityStructure_192/ActivityStructure_192.cpp
+++ ActivityStructure_192/ActivityStructure_192.cpp
@@ -18,6 +18,21 @@ int main()
 	mhs1.alamat = "Tapis";
 	mhs1.umur = 22;
 
-} 
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+	cout << "\nNim : " << mhs1.nim;
+	cout << "\nNama : " << mhs1.nama;
+	cout << "\Alamat : " << mhs1.alamat;
+	cout << "\numur : " << mhs1.umur;
+
+	
+}
 
 		

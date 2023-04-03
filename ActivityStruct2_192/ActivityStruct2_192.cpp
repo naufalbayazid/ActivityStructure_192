ff409110f82a5ed00f9134c0ae447eb7e276e91b
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-04-03 15:02:13 +0700

    masukkan alamat detail

--- ActivityStruct2_192/ActivityStruct2_192.cpp
+++ ActivityStruct2_192/ActivityStruct2_192.cpp
@@ -14,3 +14,22 @@ struct Mahasiswa {
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main () {
+
+	Mahasiswa mhs;
+
+	cout << "Masukkan nim: ";
+	cin >> mhs.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs.nama;
+	cout << "Masukkan Alamat: ";
+	cout << "\tNama desa : ";
+	cout << "Alamat:: " << endl;
+	cout << "\tNama desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\tNama kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Masukkan umur : ";
+	cin >> mhs.umur;
+

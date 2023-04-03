8961c69aa1d93fbf81270dd1b5165530b5494c0b
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-04-03 14:31:04 +0700

    masukkan data mahasiswa

---
+++ ActivityStructure_192/ActivityStructure_192.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20220140191";
+	mhs1.nama = "Codet";
+	mhs1.alamat = "Tapis";
+	mhs1.umur = 22;
+
+} 
+
+		
